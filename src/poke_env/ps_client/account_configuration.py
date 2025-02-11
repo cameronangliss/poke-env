d9fef995dbdcb8a4b39c673ccb5f5ac7b4ebd5e7
Cameron Angliss <cameronangliss99@gmail.com>
2025-02-10 21:58:51 -0800

    fix

--- src/poke_env/ps_client/account_configuration.py
+++ src/poke_env/ps_client/account_configuration.py
@@ -13,9 +13,9 @@ class AccountConfiguration(NamedTuple):
 
     username: str
     password: Optional[str]
-    char_space: str = string.ascii_lowercase + string.digits
 
     @classmethod
     def generate_config(cls, length: int) -> AccountConfiguration:
-        username = "".join(random.choices(cls.char_space, k=length))
+        char_space = string.ascii_lowercase + string.digits
+        username = "".join(random.choices(char_space, k=length))
         return AccountConfiguration(username, None)
